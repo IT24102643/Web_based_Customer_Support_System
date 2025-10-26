0953166475a526edc0699a043757aa86977c18ec
Senal Anuraheesaera <senalanuraheesara@gmail.com>
2025-10-26 15:09:58 +0530

    initial comit

---
+++ src/main/java/com/example/web_based_customer_support_system/WebBasedCustomerSupportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_based_customer_support_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedCustomerSupportSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBasedCustomerSupportSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_based_customer_support_system/WebBasedCustomerSupportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_based_customer_support_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBasedCustomerSupportSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
